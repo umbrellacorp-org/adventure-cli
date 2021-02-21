c30e57bfc9ee858513c6d0f3f9e767ab79e23771
Lars Krieger <lars.krieger@gmail.com>
2021-02-21 13:48:41 +0000

    add: createCharacter flow

--- src/commands/character/index.ts
+++ src/commands/character/index.ts
@@ -1,6 +1,9 @@
+import chalk from "chalk";
 import inquirer, { QuestionCollection } from "inquirer";
-import { Character } from "src/models/character";
+import { dialog, DialogTemper, readFile, NpcNames } from "../../utils";
+import { Character, CharacterClass, CharacterStats, classColorScheme } from "../../models/character";
 import { validateCharacterName } from "./validation"
+
 /**
  * selectCharacter
  * @description prompts the user to select their playable character form a simple list menu.
@@ -38,8 +41,48 @@ export const createCharacter = async (): Promise<Character> => {
   const { name } = await inquirer.prompt<{ name: string }>([
     { name: "name", message: "Hello adventure, what may I call you?", type: "input", validate: validateCharacterName }
   ])
-
   
+  const classChoices = [CharacterClass.WARRIOR, CharacterClass.MAGICIAN, CharacterClass.RANGER, CharacterClass.ROGUE, CharacterClass.PRIEST];
+  const { classSelection } = await inquirer.prompt<{ classSelection: CharacterClass }>([
+    { name: "classSelection", message: `Great scott! you are ${name} the, the, the ....`, type: "list", choices: classChoices }
+  ]);
 
-  return { name } as Character;
+  dialog(NpcNames.STORYTELLER, `
+    Ah yes, sorry I had forgotten it is not easy being 768 years.
+    Welcome to Kashan ${chalk.hex(classColorScheme[classSelection].hex).italic(`${classSelection}`)}, I look forward to hear of yor tales in this barren land.
+  `, DialogTemper.HAPPY);
+
+
+  const character: Character = {
+    name,
+    class: classSelection,
+    stats: {
+      strength: 10,
+      intelligence: 10,
+      dexterity: 10
+    },
+    level: 1,
+    experience: 0,
+    nextLevelExperience: 100,
+  }
+
+  return character
+}
+
+/**
+ * findCharacterByName
+ * @param {string} name Name of the character you are trying to find
+ */
+export const findCharacterByName = async (name: string): Promise<Character|undefined> => {
+  let storedCharacters: string;
+  
+  try {
+    storedCharacters = await readFile(`${__dirname}/../../data/characters.json`, "utf-8");
+  } catch (error) {
+    console.error("failed to retrieve character list")
+    return undefined;
+  }
+
+  const characterList: Character[] = JSON.parse(storedCharacters).characters;
+  return characterList.find(c => c.name.toLowerCase().trim() === name.toLowerCase().trim())
 }
--- src/commands/character/validation.ts
+++ src/commands/character/validation.ts
@@ -1,7 +1,25 @@
-import { dialog, DialogTemper } from "../../utils";
+import chalk from "chalk";
+import { findCharacterByName } from ".";
+import { dialog, DialogTemper, NpcNames } from "../../utils";
 
 export const validateCharacterName = async (input: string) => {
   const name = input.trim();
+  const character = await findCharacterByName(name);
+
+  if (!!character) {
+    dialog(NpcNames.STORYTELLER, `
+      I'm sorry i think my old ears deceived me, did you say: ${name}?
+    `, DialogTemper.ANGRY)
+    dialog(name, `
+      Yes, I'm the great adventure ${name}...
+    `, DialogTemper.NEUTRAL)
+    dialog(NpcNames.STORYTELLER, `
+      I'm so sorry to that, impersonation is punishable by death! I've already met ${name}!
+      I'm sorry.... I have to obey the rules.... yells: ${chalk.bold.italic(" Impediminio Immorectus ")}
+    `, DialogTemper.ANGRY)
+
+    return console.log(chalk.red.bold("*Puff* you have evaporated from Kashan"));
+  }
 
   if (!name || name.length < 2) {
     return dialog("Storyteller", "Sorry adventure I'm not sure i understand your name.", DialogTemper.HAPPY)
