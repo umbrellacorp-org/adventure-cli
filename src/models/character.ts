3575cc63d4252d819cb8eec15524c808b4925aa6
Lars Krieger <lars.krieger@gmail.com>
2021-02-21 13:42:15 +0000

    update: model charcater colorSchems, nextLevelExperience

--- src/models/character.ts
+++ src/models/character.ts
@@ -6,10 +6,40 @@ export enum CharacterClass {
   PRIEST = "Priest",
 }
 
+export type CharacterStats = {
+  strength: number;
+  intelligence: number;
+  dexterity: number;
+}
+
 export type Character = {
   name: string;
   class: CharacterClass;
   level: number;
+  stats: CharacterStats;
   experience: number;
-  nextLevelExperience: number;
+  nextLevelExperience: number
+}
+
+export const classColorScheme = {
+  [CharacterClass.WARRIOR]: {
+    hex: "#c7433f",
+    text: "#c7433f"
+  },
+  [CharacterClass.ROGUE]: {
+    hex: "#ea6834",
+    text: "ea6834"
+  },
+  [CharacterClass.MAGICIAN]: {
+    hex: "#245fca",
+    text: "#245fca"
+  },
+  [CharacterClass.RANGER]: {
+    hex: "#72b357",
+    text: "#72b357"
+  },
+  [CharacterClass.PRIEST]: {
+    hex: "#ece83d",
+    text: "#ece83d"
+  },
 }
